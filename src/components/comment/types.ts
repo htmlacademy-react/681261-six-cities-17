03ea836837a4565dd579c713b56070d7cb72b44f
stasnovikov <allenwalker12349@gmail.com>
2024-12-14 19:00:04 +0300

    4.21. Обратная сторона реальности

---
+++ src/components/comment/types.ts
@@ -0,0 +1,8 @@
+export interface CommentItemType {
+  id: string;
+  userName: string;
+  avatarUrl: string;
+  rating: number;
+  text: string;
+  date: string;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app.tsx';
+import { offers } from './mocks/offers.ts';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <App count={2}/>
+    <App offers={offers} />
   </React.StrictMode>
 );
